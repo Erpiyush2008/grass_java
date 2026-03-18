a1f388b3bf947a10c4adb351451fb806b234fbe6
Erpiyush2008 <piyushsharma190708@gmail.com>
2026-03-18 09:40:14 +0530

    hell

--- object_class.java
+++ object_class.java
@@ -88,46 +88,49 @@
 // Creates and returns a copy of this object. The precise meaning of "copy" may depend on the class of the object. The general intent is that, for any object x, the expression:
 
 
-// class Student implements Cloneable {
-//     int id = 10;
+class Student implements Cloneable {
+    int id = 10;
 
-//     protected Object clone() throws CloneNotSupportedException {
-//         return super.clone();
-//     }
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 
-// }
-// public class object_class{
-//     public static void main(String[] args) throws Exception {
-//         Student s1 = new Student();
-//         Student s2 = (Student) s1.clone();
+}
+public class object_class{
+    public static void main(String[] args) throws Exception {
+        Student s1 = new Student();
 
-//         System.out.println(s2.id);
-//     }
-// }
+        Student s2 = (Student) s1.clone();
+
+        System.out.println(s1.id);
+    }
+}
 
 
 
 //
-class object_class {
-    public static void main(String[] args) throws Exception {
 
-        Object obj = new Object();
 
-        Thread t = new Thread(() -> {
-            synchronized (obj) {
-                try {
-                    // obj.wait();
-                    System.out.println("Thread resumed");
-                } catch (Exception e) {}
-            }
-        });
+// class object_class {
+//     public static void main(String[] args) throws Exception {
 
-        t.start();
+//         Object obj = new Object();
 
-        // Thread.sleep(2000);
+//         Thread t = new Thread(() -> {
+//             synchronized (obj) {
+//                 try {
+//                     // obj.wait();
+//                     System.out.println("Thread resumed");
+//                 } catch (Exception e) {}
+//             }
+//         });
 
-        // synchronized (obj) {
-        //     obj.notify();
-        // }
-    }
-}
+//         t.start();
+
+//         // Thread.sleep(2000);
+
+//         // synchronized (obj) {
+//         //     obj.notify();
+//         // }
+//     }
+// }
