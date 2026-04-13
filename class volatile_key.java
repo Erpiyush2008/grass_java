00796365fa6d844bbf738549140086e2fa2fa538
Erpiyush2008 <piyushsharma190708@gmail.com>
2026-04-13 23:03:51 +0530

    hello

---
+++ class volatile_key.java
@@ -0,0 +1,17 @@
+class vol {
+    static volatile boolean flag = false;
+
+    public static void main(String[] args) {
+        
+        new Thread(() -> {
+            while (!flag) {
+                // will stop correctly ✅
+            }
+            System.out.println("Stopped");
+        }).start();
+
+        new Thread(() -> {
+            flag = true;
+        }).start();
+    }
+}
